3fea4f4a6833f72480a760a86cd679f511cc233f
elisejakob <hei@elisejakob.no>
2021-10-19 18:36:33 +0200

    add front page alts

---
+++ web/src/pages/forside2.vue
@@ -0,0 +1,24 @@
+<template>
+  <Layout overlay>
+    <Hero />
+    <ProjectGrid />
+    <Instafeed />
+  </Layout>
+</template>
+
+<script>
+import Hero from '@/components/Hero'
+import Instafeed from '@/components/Instafeed'
+import ProjectGrid from '@/components/projects/ProjectGrid2'
+
+export default {
+  components: {
+    Hero,
+    Instafeed,
+    ProjectGrid
+  },
+  metaInfo: {
+    title: 'Ørseng Interiørarkitektur'
+  }
+}
+</script>
